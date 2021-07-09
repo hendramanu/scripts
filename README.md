e9795b0e7ebe7e195b0f1b6a744c0ba187d27abd
Hendra Manudinata <aku.hendramanu@gmail.com>
2021-07-09 17:12:59 +0700

    Add script for setting up environment for Android build

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scripts
 Some scripts, for this and that and these and those
+
+## setup-android.sh
+Setup environment for Android building
---
+++ setup-android.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Setup environment for Android building
+
+# This script only for CI
+if [[ -z $CI ]]; then
+	echo -e "\nThis script only for CI!\n"
+	exit 1
+fi
+
+# Install packages
+apt-get install -y \
+	bison build-essential curl flex \
+        git gnupg gperf liblz4-tool \
+        libncurses5-dev libsdl1.2-dev \
+        libxml2 libxml2-utils lzop \
+        pngcrush schedtool squashfs-tools \
+        xsltproc zip zlib1g-dev \
+        build-essential kernel-package \
+        libncurses5-dev \
+        bzip2 git python wget \
+        gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+
+# Setup Git identity
+GIT_USERNAME=$(git config --global --get user.name)
+GIT_EMAIL=$(git config --global --get user.email)
+GIT_COLORUI=$(git config --global --get color.ui)
+
+
+if [[ -z $GIT_USERNAME ]]; then
+	git config --global user.name "Hendra Manudinata"
+elif [[ -z $GIT_EMAIL ]]; then
+	git config --global user.email "aku.hendramanu@gmail.com"
+elif [[ -z $GIT_COLORUI ]]; then
+	git config --global color.ui false
+fi
+
+# Clone compiler
+# AOSP Clang 4639204
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/android-9.0.0_r6/clang-4639204.tar.gz && mkdir clang-4639204 && tar -zxvf clang-4639204.tar.gz -C clang-4639204 && rm clang-4639204.tar.gz.tar.gz
+
+# AOSP gcc 4.9
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc49-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc49-32
